9b0932cbccb1f14227908e07b0c632272470ba9d
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2022-04-04 15:12:45 -0500

    Make PINN execution more efficient
    Use Dagger global context changes
    Run DaggerWebDash visualizer
    Use 1 BLAS thread
    Add SumLayer

--- run.jl
+++ run.jl
@@ -1,7 +1,39 @@
 using NeuralPDE, Flux, ModelingToolkit, GalacticOptim, Optim, DiffEqFlux
 using Quadrature, Cuba, CUDA, QuasiMonteCarlo
 import ModelingToolkit: Interval, infimum, supremum
-using DaggerFlux
+
+using ChainRulesCore, Zygote
+
+using DaggerFlux, Dagger, DaggerWebDash
+using DataFrames
+
+ml = Dagger.MultiEventLog()
+ml[:core] = Dagger.Events.CoreMetrics()
+ml[:id] = Dagger.Events.IDMetrics()
+ml[:wsat] = Dagger.Events.WorkerSaturation()
+ml[:loadavg] = Dagger.Events.CPULoadAverages()
+ml[:bytes] = Dagger.Events.BytesAllocd()
+ml[:mem] = Dagger.Events.MemoryFree()
+ml[:esat] = Dagger.Events.EventSaturation()
+ml[:psat] = Dagger.Events.ProcessorSaturation()
+lw = Dagger.Events.LogWindow(5*10^9, :core)
+df = DataFrame([key=>[] for key in keys(ml.consumers)]...)
+ts = Dagger.Events.TableStorage(df)
+push!(lw.creation_handlers, ts)
+d3r = DaggerWebDash.D3Renderer(8080; seek_store=ts)
+push!(lw.creation_handlers, d3r)
+push!(lw.deletion_handlers, d3r)
+push!(d3r, DaggerWebDash.GanttPlot(:core, :id, :timeline, :esat, :psat, "Overview"))
+push!(d3r, DaggerWebDash.LinePlot(:core, :wsat, "Worker Saturation", "Running Tasks"))
+push!(d3r, DaggerWebDash.LinePlot(:core, :loadavg, "CPU Load Average", "Average Running Threads"))
+push!(d3r, DaggerWebDash.LinePlot(:core, :bytes, "Allocated Bytes", "Bytes"))
+push!(d3r, DaggerWebDash.LinePlot(:core, :mem, "Available Memory", "% Free"))
+ml.aggregators[:logwindow] = lw
+ml.aggregators[:d3r] = d3r
+Dagger.global_context().log_sink = ml
+
+using LinearAlgebra
+BLAS.set_num_threads(1)
 
 @parameters t x y
 @variables u(..)
@@ -33,26 +65,35 @@ domains = [t ∈ Interval(t_min,t_max),
 
 # Neural network
 
-# Automatically calls `collect` on a `DaggerChain`
-struct AutoChain
-    inner
+struct SumLayer{L<:Tuple}
+    layers::L
 end
-(ac::AutoChain)(x...) = collect(ac.inner(x...))
+#(par::SumLayer)(x) = reduce((x,y)-> x .+ y, map(l -> l(x), par.layers))
+(par::SumLayer)(ip) =
+    Dagger.delayed(xs -> reduce((x,y) -> x .+ y, xs))([DaggerFlux.daglayer(f, ip) for f in par.layers])
+Zygote.@adjoint function (sl::SumLayer)(x)
+    return sl(x), dy -> (nothing, dy)
+end
+DaggerFlux.daglayer(par::SumLayer, ip) =
+    Dagger.delayed(xs -> reduce((x,y) -> x .+ y, xs))([DaggerFlux.daglayer(f, ip) for f in par.layers])
 
-inner = 25
+inner = 1000
 chain = Chain(Dense(3,inner,Flux.σ),
-              Dense(inner,inner,Flux.σ),
-              Dense(inner,inner,Flux.σ),
-              Dense(inner,inner,Flux.σ),
+              #SumLayer((
+                Dense(inner,inner,Flux.σ),
+                Dense(inner,inner,Flux.σ),
+                Dense(inner,inner,Flux.σ),
+              #)),
               Dense(inner,1))
-chain = AutoChain(DaggerChain(chain))
+chain = DaggerChain(chain)
+DiffEqFlux.initial_params(f::DaggerChain) = Flux.destructure(f)[1]
 #= TODO: Use FastChain
 chain = FastChain(FastDense(3,inner,Flux.σ),
                   FastDense(inner,inner,Flux.σ),
                   FastDense(inner,inner,Flux.σ),
                   FastDense(inner,inner,Flux.σ),
                   FastDense(inner,1))
-chain = AutoChain(DaggerChain(Chain(chain)))
+chain = DaggerChain(Chain(chain))
 
 #initθ = CuArray(Float64.(DiffEqFlux.initial_params(chain)))
 initθ = Float64.(DiffEqFlux.initial_params(chain))
